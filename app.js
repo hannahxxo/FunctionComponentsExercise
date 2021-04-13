416618a2f6aac6031703a2252ccfba74f78b8c09
hannahxxo <hannahantle@gmail.com>
2021-04-13 09:37:10 -0500

    Completed Exercise with Corrections

--- app.js
+++ app.js
@@ -5,13 +5,13 @@ const GrinchXmas = () => {
 
 // 1b
 ReactDOM.render(
-    <GrinchXmas />,
+    <GrinchXmas/>,
     document.getElementById('root')
 );
 
 // 2a
 const GrinchMusic = () => {
-    return <h1>Blast this Christmas music! It's joyful and triumphant.</h1>
+    return <h1>Blast this Christmas music! It's joyful and triumphant.</h1>;
 }
 
 // 2b
@@ -24,8 +24,8 @@ ReactDOM.render(
 const GrinchIdiot = () => {
     return (
         <div>
-            <h1>"I'm an idiot!"</h1>
-            <h1>"You're an idiot!"</h1>
+            <h2>"I'm an idiot!"</h2>
+            <h2>"You're an idiot!"</h2>
         </div>
     );
 }
@@ -41,14 +41,14 @@ const GrinchSchedule = () => {
     return (
     <div>
         <ul>
-            <li>"4:00, wallow in self-pity."</li>
-            <li>"4:30, stare into the abyss."</li>
-            <li>"5:00, solve world hunger, tell no one."</li>
-            <li>"5:30, jazzercize."</li>
-            <li>"6:30, dinner with me. I can't cancel that again."</li>
-            <li>"7:00, wrestle with my self-loathing."</li>
+            <li>4:00, wallow in self-pity.</li>
+            <li>4:30, stare into the abyss.</li>
+            <li>5:00, solve world hunger, tell no one.</li>
+            <li>5:30, jazzercize.</li>
+            <li>6:30, dinner with me. I can't cancel that again.</li>
+            <li>7:00, wrestle with my self-loathing.</li>
         </ul>
-        <p>"I'm booked. Of course, if I bump the loathing to 9, I could still be done in time to lay in bed, stare at the ceiling and slip slowly into madness."</p>
+        <p>I'm booked. Of course, if I bump the loathing to 9, I could still be done in time to lay in bed, stare at the ceiling and slip slowly into madness.</p>
     </div>
     );
 }
@@ -61,8 +61,10 @@ ReactDOM.render(
 
 // 5a
 const GrinchPoster = () => {
-    return 
-        <img src="https://img01.mgo-images.com/image/thumbnail/v2/content/1MV1ca3f097dd209568db5d4a36e429dbda.jpeg" alt="" srcset="" />;
+    return <img
+    src="https://img01.mgo-images.com/image/thumbnail/v2/content/1MV1ca3f097dd209568db5d4a36e429dbda.jpeg"
+    alt="The Grinch Movie Poster"
+    />;
 }
 
 // 5b
@@ -73,7 +75,9 @@ ReactDOM.render(
 
 // 6a
 const GrinchVmail = () => {
-    const u = "YOU"
+    // 6b
+    const u = "YOU";
+    // 6c
     return (
         <div>
             <h1>"IF {u} UTTER SO MUCH AS ONE SYLLABLE, I'LL HUNT {u} DOWN AND GUT {u} LIKE A FISH!"</h1>
@@ -82,7 +86,7 @@ const GrinchVmail = () => {
     );
 }
 
-// 6b
+// 6d
 ReactDOM.render(
     <GrinchVmail />,
     document.getElementById('root')
@@ -92,12 +96,12 @@ ReactDOM.render(
 const GrinchAll = () => {
     return (
         <div>
-            <GrinchXmas />,
-            <GrinchMusic />,
-            <GrinchIdiot />,
-            <GrinchSchedule />,
-            <GrinchPoster />,
-            <GrinchVmail />,
+            <GrinchXmas />
+            <GrinchMusic />
+            <GrinchIdiot />
+            <GrinchSchedule />
+            <GrinchPoster />
+            <GrinchVmail />
         </div>
     );
 }
@@ -108,10 +112,21 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+// BONUS
 // 8a
 const DiceRoll = () => {
     // 8b
-    const die1 = (Math.random*6)Math.floor;
+    const die1 = Math.floor(6 * Math.random()) + 1;
     // 8c
-    const die2 = (Math.random*6)Math.floor;
+    const die2 = Math.floor(6 * Math.random()) + 1;
+    // 8d
+    return (
+        <h1>{(die1 === 1 && die2 === 1) ? "SNAKE EYES" : `${die1} & ${die2}`}</h1>
+    );   
 }
+
+// 8e
+ReactDOM.render(
+    <DiceRoll/>,
+    document.getElementById('root')
+);
